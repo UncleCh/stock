b451d481a7c074f07a9112dc00e76f5a43384761
uncle chen <15623044255@163.com>
2017-07-23 22:09:31 +0800

    Update constant.py

--- py/com/constant.py
+++ py/com/constant.py
@@ -1,1 +1,10 @@
 stock_code = 600129
+date_format = "%Y-%m-%d"
+type_open = 1  # open + close / 2
+type_high = 2 # high
+type_three = 3 # open + close + high / 3
+# db config
+db_host =
+db_port = 27017
+db_database = "stock"
+db_tushare_collection = "tushare_stock"
