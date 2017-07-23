c2e5861ca9c8906fff5d473e86c42b6ae0d8116d
uncle chen <15623044255@163.com>
2017-07-23 22:31:26 +0800

    Update constant.py

--- py/com/constant.py
+++ py/com/constant.py
@@ -4,7 +4,8 @@ type_open = 1  # open + close / 2
 type_high = 2 # high
 type_three = 3 # open + close + high / 3
 # db config
-db_host =
+db_host = 
 db_port = 27017
 db_database = "stock"
 db_tushare_collection = "tushare_stock"
+db_forecast_collection = "forecast"
